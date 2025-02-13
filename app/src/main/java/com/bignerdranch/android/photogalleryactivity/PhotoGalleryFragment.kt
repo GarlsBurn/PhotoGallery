ab1e23ee8913579dcfc5e35f0a3fdb28a7cd2c78
Ilya Belikov <belikovilya0@gmail.com>
2025-02-13 20:50:50 +0300

    Прослушивание жизненного цикла представления.Передача и использование обработчиков

--- app/src/main/java/com/bignerdranch/android/photogalleryactivity/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/PhotoGalleryFragment.kt
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.photogalleryactivity
 
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -39,8 +41,14 @@ class PhotoGalleryFragment: Fragment() {
 
         retainInstance = true
 
-        thumbnailDownloader = ThumbnailDownloader()
-        lifecycle.addObserver(thumbnailDownloader)
+        val responseHandler = Handler()
+        thumbnailDownloader =
+            ThumbnailDownloader(responseHandler){photoHolder, bitmap ->
+                val drawable = BitmapDrawable(resources, bitmap)
+                photoHolder.bindDrawable(drawable)
+
+            }
+        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
     }
 
     override fun onCreateView(
@@ -49,7 +57,7 @@ class PhotoGalleryFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
-
+        viewLifecycleOwner.lifecycle.addObserver(thumbnailDownloader.viewLifecycleObserver)
         photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
         return view
@@ -66,10 +74,17 @@ class PhotoGalleryFragment: Fragment() {
         )
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewLifecycleOwner.lifecycle.removeObserver(
+            thumbnailDownloader.viewLifecycleObserver
+        )
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         lifecycle.removeObserver(
-            thumbnailDownloader
+            thumbnailDownloader.fragmentLifecycleObserver
         )
     }
 
