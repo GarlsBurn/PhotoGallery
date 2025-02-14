425d58825410274a1cfefc8cbe8d6df7573d4b96
Ilya Belikov <belikovilya0@gmail.com>
2025-02-14 23:06:46 +0300

    Регистрация событий SearchView.OnQueryTextListener. Хранение последнего запроса в PhotoGalleryViewModel

--- app/src/main/java/com/bignerdranch/android/photogalleryactivity/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/PhotoGalleryViewModel.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.photogalleryactivity
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import retrofit2.http.Query
@@ -13,13 +14,23 @@ class PhotoGalleryViewModel: ViewModel() {
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
 
+
+
     init {
         mutableSearchTerm.value = "car"
 
-        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
-            flickrFetchr.searchPhotos(searchTerm)
+
+         galleryItemLiveData = MediatorLiveData<List<GalleryItem>>().apply {
+            addSource(mutableSearchTerm){
+                searchTerm -> val liveData = flickrFetchr.searchPhoto(searchTerm)
+                addSource(liveData){
+                    photo -> value = photo
+                    removeSource(liveData)
+                }
+            }
         }
     }
+
 
     fun fetchPhotos(query: String = ""){
         mutableSearchTerm.value = query
---
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/QueryPreferences.kt
@@ -0,0 +1,5 @@
+package com.bignerdranch.android.photogalleryactivity
+
+class QueryPreferences {
+
+}
