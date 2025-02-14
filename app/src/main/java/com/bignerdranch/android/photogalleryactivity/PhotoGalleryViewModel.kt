6cf8cc2d937a3ad61271b30694be904c76f8d6cb
Ilya Belikov <belikovilya0@gmail.com>
2025-02-15 00:16:52 +0300

    Некоторые изменения к предыдущему коммиту

--- app/src/main/java/com/bignerdranch/android/photogalleryactivity/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/PhotoGalleryViewModel.kt
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogalleryactivity
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,22 +9,28 @@ import androidx.lifecycle.ViewModel
 import retrofit2.http.Query
 
 
-class PhotoGalleryViewModel: ViewModel() {
+class PhotoGalleryViewModel(private val app: Application): AndroidViewModel(app) {
 
     val galleryItemLiveData: LiveData<List<GalleryItem>>
 
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
 
+    val searchTerm: String
+        get() = mutableSearchTerm.value ?:""
 
 
     init {
-        mutableSearchTerm.value = "car"
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
 
 
          galleryItemLiveData = MediatorLiveData<List<GalleryItem>>().apply {
             addSource(mutableSearchTerm){
-                searchTerm -> val liveData = flickrFetchr.searchPhoto(searchTerm)
+                searchTerm -> val liveData =
+                    if ( searchTerm.isBlank()){flickrFetchr.fetchPhotos()
+                    } else {
+                        flickrFetchr.searchPhoto(searchTerm)
+                    }
                 addSource(liveData){
                     photo -> value = photo
                     removeSource(liveData)
@@ -33,6 +41,7 @@ class PhotoGalleryViewModel: ViewModel() {
 
 
     fun fetchPhotos(query: String = ""){
+        QueryPreferences.setStoredQuery(app, query)
         mutableSearchTerm.value = query
     }
 }
